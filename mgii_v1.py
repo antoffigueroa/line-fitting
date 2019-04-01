1ebed2c34a61bd0105dd4eb78b322eab099307a9
Antonia Fernandez Figueroa <antonia@MacBook-Air-de-Antonia.local>
2019-04-01 16:46:01 -0300

    more conditions added to the fits

--- mgii_v1.py
+++ mgii_v1.py
@@ -5,6 +5,8 @@ from functions_mgii import *
 from mpdaf.obj import Spectrum
 from scipy.optimize import bisect, leastsq
 import scipy.stats
+import aplpy
+from astropy.io import fits
 
 cube=Cube('/Users/antonia/taller-de-investigacion/SGASJ1226/muse/r4_cube_mgii_mock.fits')
 
@@ -22,13 +24,27 @@ coord = np.zeros((ncoords,2),dtype=np.int)
 aux = 0
 
 # j es x, i es y
-for i in range(43, 44):
-    for j in range(45,46):
+# very bright spaxel, used for experimentation
+#for i in range(43, 44):
+#    for j in range(45,46):
+#        coord[aux] = i,j
+#        aux = aux + 1
+#ncoords=aux
+
+# select a rectangular region to iterate
+
+for i in range(32, 52):
+    for j in range(32,48):
         coord[aux] = i,j
         aux = aux + 1
+
 ncoords=aux
 
 contador = 0
+
+EW = np.array([])
+x_succes = np.array([])
+y_succes = np.array([])
 
 for k in range(ncoords):
     y = coord[k,1] #q[i]
@@ -46,16 +62,21 @@ for k in range(ncoords):
     flux_2, err_flux_2 = flux(A_2_fit, err_A_2)
     EW_1, err_EW_1 = eq_width(flux_1,err_flux_1,z)
     EW_2, err_EW_2 = eq_width(flux_2,err_flux_2,z)
-    if EW_1/err_EW_1 >= 2.5:
-    	print "succesful fit at: "+str(x)+", "+str(y)
+    if EW_1/err_EW_1 >= 2.5 and A_1_fit > 0 and A_2_fit > 0 and A_1_fit/A_2_fit < 2:
+    	print "succesful fit at: "+str(x)+", "+str(y)+' with EW: '+str(EW_1)
         contador = contador +1
+        #best_fit = double_model((A_1_fit, A_2_fit, mu_fit), np.linspace(spe.wave.coord()[0], spe.wave.coord()[-1], num=100000))
+        #plt.figure()
+        #plt.plot(np.linspace(spe.wave.coord()[0], spe.wave.coord()[-1], num=100000), best_fit)
+        #plt.plot(spe.wave.coord(), spe.data)
+        #plt.show()
     else:
         print ":("
 
 best_fit = double_model((A_1_fit, A_2_fit, mu_fit), np.linspace(spe.wave.coord()[0], spe.wave.coord()[-1], num=100000))
 
-plt.figure()
-plt.plot(np.linspace(spe.wave.coord()[0], spe.wave.coord()[-1], num=100000), best_fit)
-plt.plot(spe.wave.coord(), spe.data)
-plt.show()
-#print contador
+#plt.figure()
+#plt.plot(np.linspace(spe.wave.coord()[0], spe.wave.coord()[-1], num=100000), best_fit)
+#plt.plot(spe.wave.coord(), spe.data)
+#plt.show()
+print contador
